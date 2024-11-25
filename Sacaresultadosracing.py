a55f6a7dfd9b579cdd39c0df6f33c284ffc0658d
batjuancrespo <batjuancrespo@gmail.com>
2024-11-25 19:41:47 +0100

    Update Sacaresultadosracing.py

--- Sacaresultadosracing.py
+++ Sacaresultadosracing.py
@@ -23,7 +23,7 @@ def scrape_racing_results():
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="latest").install()), options=chrome_options)
 
     # Variable para almacenar los resultados
     result_text = ""
