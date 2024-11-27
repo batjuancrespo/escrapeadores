ccee59a8f2a53fb57a8067a37e0adb78400fa8dc
batjuancrespo <batjuancrespo@gmail.com>
2024-11-27 17:46:04 +0100

    Update Sacaresultadosmadridfut.py

--- Sacaresultadosmadridfut.py
+++ Sacaresultadosmadridfut.py
@@ -23,7 +23,7 @@ def scrape_real_madrid_results():
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
-    service = Service(ChromeDriverManager(version="latest").install())
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     # Variable para almacenar los resultados
